b5dbd4234e9f31618701887ca174441cf16aaf2f
Nikita Kolesnik <118283668+KolesnikNV@users.noreply.github.com>
2023-10-14 23:45:29 +0400

    Change README

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,4 +3,7 @@ set -x
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --noinput
+export DJANGO_SUPERUSER_PASSWORD=adminpassword
+python manage.py createsuperuser --noinput --username=admin2 --email=admin2@example.com
+unset DJANGO_SUPERUSER_PASSWORD
 gunicorn crm.wsgi:application --bind 0.0.0.0:8000
